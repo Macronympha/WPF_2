699e125d12584c969a585f7d42447716d3d0587c
Rina <irina.alman98@gmail.com>
2022-02-07 12:39:51 +0700

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class helper
+        {
+            public static WSR012Entities ent;
+
+            public static int priority = 0;
+            public static bool flag = false;
+
+            public static WSR012Entities GetContext()
+            {
+            if (ent == null)
+                {
+                    ent = new WSR012Entities();
+                }
+            return ent;
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            frame.Content = new Page1(frame);
+        }
+
+        private void frame_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            try
+            {
+                Page1 pg = (Page1)e.Content;
+                pg.Load();
+            }
+            catch { };
+        }
+    }
+}
